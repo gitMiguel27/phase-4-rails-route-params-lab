d19a3054ea95c63254c56beee383b6c76b75ae4c
Miguel Nazario <mnazario35@gmail.com>
2022-04-04 17:11:24 -0400

    commit tree 3ade177abd8b4abae665ae55df79038c8e75a306

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,12 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.find_by(first_name: params[:name1], last_name: params[:name2])
     render json: students
   end
 
+  def show
+    student = Student.all.find_by(id: params[:id])
+    render json: student
+  end
 end
